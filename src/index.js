034f43b233b4562e20299a846e5b2edbede8926d
haidang <haidang02032003@gmail.com>
2023-02-13 23:20:58 +0700

    static file and scss

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {engine} = require('express-handlebars')
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+
+// Template engine
+app.engine('hbs', engine({extname: '.hbs'})); // hbs new 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// method
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/tin-tuc', (req, res) => {
+  res.render('tintuc');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
